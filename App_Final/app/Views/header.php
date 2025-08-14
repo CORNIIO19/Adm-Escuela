6fe84e2c9183017765ee076a9ef6c2f917694073
Carlos Adrian Abad Segundo <carlos.abad@cua.uam.mx>
2025-08-13 21:17:06 -0600

    conexion con usuarios
    se hace la coneccion para funciones exculivas de usuarios profesir, administrador y control escolar

--- App_Final/app/Views/header.php
+++ App_Final/app/Views/header.php
@@ -5,6 +5,11 @@
             <?php if(isset($user) && ($user['tipo'] === 'Administrador' || $user['tipo'] === 'Control Escolar')): ?>
                 <li><a href="?url=CrearCuenta">Crear Cuenta</a></li>
             <?php endif; ?>
+            <li><a href="?url=Home">Inicio</a></li>
+    <?php if (isset($user) && in_array($user['tipo'], ['Administrador', 'Control Escolar', 'Profesor'])): ?>
+        <li><a href="?url=../Controllers/index.php">Gestión Académica</a></li>
+    <?php endif; ?>
+
             <li><a href="?url=HistorialPagos/index">Historial de Pagos</a></li>
             <li><a href="?url=RegistrarPago/form">Registrar Pago</a></li>
             <li><a href="?url=Auth/logout">Cerrar Sesión</a></li>
